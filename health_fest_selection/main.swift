c3364590780849ae5e8a60df747109b2d3cdc02e
stringconstant <wdu@students.stab.org>
2017-11-13 09:41:00 -0500

    Initial Commit

---
+++ health_fest_selection/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  health_fest_selection
+//
+//  Created by William Du on 2017/11/13.
+//  Copyright © 2017年 William Du. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
